0ab6d919f3507f484bb1d8e991ddf69174936040
Simon Waight <simon.waight@gmail.com>
2022-11-03 02:03:03 +0000

    Add new SDK references

--- src/main/java/com/siliconvalve/demo/Function.java
+++ src/main/java/com/siliconvalve/demo/Function.java
@@ -9,6 +9,9 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
+import com.azure.communication.email.models.*;
+import com.azure.communication.email.*;
+
 import com.apptasticsoftware.rssreader.*;
 
 import java.io.IOException;
@@ -21,6 +24,15 @@ import java.util.stream.Stream;
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
+
+
+    // @FunctionName("TimerTrigger")
+    // public void timerHandler(
+    //     @TimerTrigger(name = "timerInfo", schedule = "0 */5 * * * *") String timerInfo,
+    //     final ExecutionContext context
+    // ) {
+
+
     /**
      * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
      * 1. curl -d "HTTP Body" {your host}/api/HttpExample
@@ -33,35 +45,49 @@ public class Function {
                 methods = {HttpMethod.GET, HttpMethod.POST},
                 authLevel = AuthorizationLevel.ANONYMOUS)
                 HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
+            // @TableInput(
+            //     name="persons", 
+            //     partitionKey="{partitionKey}", 
+            //     tableName="%MyTableName%", 
+            //     connection="MyConnectionString") Person[] persons,
+            // @TableOutput(
+            //     name="person", 
+            //     partitionKey="{partitionKey}", 
+            //     rowKey = "{rowKey}", 
+            //     tableName="%MyTableName%", 
+            //     connection="MyConnectionString") OutputBinding<Person> person,
+            final ExecutionContext context)
+             {
 
         context.getLogger().info("Java HTTP trigger processed a request.");
 
-
         StringBuilder headings = new StringBuilder("<h1>Retirement announcements</h1>");
 
+        List<Item> articles = readUpdatesRssFeed(context);
+
+        while(articles.iterator().hasNext())
+        {
+            headings.append(articles.iterator().next().getTitle() + "<br/>");
+        }
+
+        return request.createResponseBuilder(HttpStatus.OK).body(headings).build();
+    }
+
+
+    private List<Item> readUpdatesRssFeed(ExecutionContext context)
+    { 
+        List<Item> articles = null;
         RssReader reader = new RssReader();
         try 
         {
-            List<Item> articles = reader.read("https://azurecomcdn.azureedge.net/en-us/updates/feed/?updateType=retirements").collect(Collectors.toList());
-
-            while(articles.iterator().hasNext())
-            {
-                headings.append(articles.iterator().next().getTitle() + "<br/>");
-            }
+            articles = reader.read(System.getenv("UpdatesURL")).collect(Collectors.toList());
         } catch (IOException e) {
             context.getLogger().info(e.getMessage());
             e.printStackTrace();
         }
-
-        // Parse query parameter
-        // final String query = request.getQueryParameters().get("name");
-        // final String name = request.getBody().orElse(query);
-
-        // if (name == null) {
-        //     return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
-        // } else {
-            return request.createResponseBuilder(HttpStatus.OK).body(headings).build();
-        //}
+        return articles;
     }
+
+    // private boolean prepareAndSendAlertEmail()
+    // {}
 }
